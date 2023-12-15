a473615af9ad5ddea61e635f05af466dc9e66504
Ahmed Mahdy <am.elnaddar@gmail.com>
2023-12-15 02:38:03 +0200

    formated envelopes.js file

--- api/envelopes.js
+++ api/envelopes.js
@@ -4,11 +4,11 @@ const envelopes = express.Router();
 
 module.exports = envelopes;
 
-envelopes.post("/", (req, res, next) =>{
-    try{
+envelopes.post("/", (req, res, next) => {
+    try {
         const obj = db.addEnvelope(req.body);
         res.status(201).send(obj);
-    }catch(err){
+    } catch (err) {
         next(err);
     }
 });
