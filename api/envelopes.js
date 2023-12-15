627ca021218ccb730ab9c2317688866cee3835a6
Ahmed Mahdy <am.elnaddar@gmail.com>
2023-12-15 02:21:43 +0200

    created POST /envelopes

--- api/envelopes.js
+++ api/envelopes.js
@@ -3,3 +3,12 @@ const db = require("./db");
 const envelopes = express.Router();
 
 module.exports = envelopes;
+
+envelopes.post("/", (req, res, next) =>{
+    try{
+        const obj = db.addEnvelope(req.body);
+        res.status(201).send(obj);
+    }catch(err){
+        next(err);
+    }
+});
