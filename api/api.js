7305f6d6c8da58bd0741a4a4ca0c22ec1c0625bc
Ahmed Mahdy <am.elnaddar@gmail.com>
2023-12-15 02:21:14 +0200

    init envelopes endpoint

--- api/api.js
+++ api/api.js
@@ -3,7 +3,10 @@ const apiRouter = express.Router();
 
 module.exports = apiRouter;
 
+apiRouter.use("/envelopes", require("./envelopes"));
+
 apiRouter.get("/", (req, res, next)=>{
     console.log("Hello World");
     res.send();
 })
+
